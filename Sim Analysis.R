b7d8dcb7126a5afae9ea661cb65b07827a1e89de
Greb <iblakethompson@gmail.com>
2026-03-22 18:45:46 +0000

    Chnaged some names of datframes to more appropriate names.
    Began start on a parameter data frame, starting by binning parameter levels. Need to work on OV levels though

--- Sim Analysis.R
+++ Sim Analysis.R
@@ -7,14 +7,12 @@ sim_files <- list.files(
   full.names = TRUE # Keep full path names
 )
 
-sim_data_list <- vector("list", length(sim_files))
+sim_data_list <- vector("list")
 
 for (file in sim_files) {
-  loaded_names <- load(file)# Load RData file and retrieve first object as a data frame
-  data <- as.data.frame(get(loaded_names[1]))
-  
-  # Append generated parameters as new columns
-  gen_param_df <- as.data.frame(t(unlist(genParams)))
+  loaded_names <- load(file)# Load RData file 
+  data <- as.data.frame(get(loaded_names[1])) # retrieve first object as a data frame
+  gen_param_df <- as.data.frame(t(genParams)) # Append genparams as new columns
   data <- cbind(data, gen_param_df[rep(1, nrow(data)), , drop = FALSE])
   colnames(data) <- c("Time", "Resp", "OV", "DIFF", names(genParams)) # Assign column names
   sim_data_list[[file]] <- data # Store in list using file name as key
@@ -26,9 +24,9 @@ sim_data <- bind_rows(sim_data_list) |> # Bind all data in the list together
   )
 
 
-# Data Visualisation ----------------------------------------------------------
-# Summary Data ----------------------------------------------------------
-accuracy_data <- sim_data |>
+# Data Visualisation -----------------------------------------------------------
+# Stimuli Data ----------------------------------------------------------
+stimuli_data <- sim_data |>
   mutate(Resp = case_when( # Changing them for Clarity
     Resp == 1 ~ "correct",
     Resp == 2 ~ "incorrect"
@@ -44,7 +42,7 @@ accuracy_data <- sim_data |>
   ) |>
   mutate(accuracy = as.numeric(Count_correct / (Count_correct + Count_incorrect)))
 ## Accuracy ------------------------------------------------------------------
-ggplot(data = accuracy_data, aes(x = OV, y = accuracy, colour = DIFF)) +
+ggplot(data = stimuli_data, aes(x = OV, y = accuracy, colour = DIFF)) +
   geom_smooth(method = "loess") +
   geom_point() +
   scale_y_continuous(breaks = seq(.5, 1, by = .05), 
@@ -62,7 +60,7 @@ ggplot(data = accuracy_data, aes(x = OV, y = accuracy, colour = DIFF)) +
 ggsave("accuracy_plot.pdf", width = 21, height = 15,
        units = "cm", path = "~/UoL/Live-DeMo")
 
-## Response Time --------------------------------------------------------------
+## Response Time ---------------------------------------------------------------
 ### Overall Response Time----
 ggplot(data = sim_data, aes(x = OV, y = Time, colour = DIFF)) +
   geom_smooth(method = "loess") +
@@ -71,14 +69,33 @@ ggplot(data = sim_data, aes(x = OV, y = Time, colour = DIFF)) +
   theme_minimal()
 
 ### Mean Correct Response Time----
-ggplot(data = accuracy_data, aes(x = OV, y = mean_rt_correct, colour = DIFF)) +
+ggplot(data = stimuli_data, aes(x = OV, y = mean_rt_correct, colour = DIFF)) +
   geom_smooth(method = "loess") +
   geom_point() +
   theme_minimal()
 
 ### Mean Error Response Time
 
-ggplot(data = accuracy_data, aes(x = OV, y = mean_rt_incorrect, colour = DIFF)) +
+ggplot(data = stimuli_data, aes(x = OV, y = mean_rt_incorrect, colour = DIFF)) +
   geom_smooth(method = "loess") +
   geom_point() +
   theme_minimal()
+
+# Parameter Data ---------------------------------------------------------------
+level_labels <- c('low', 'med', 'high')
+
+OV_breaks <- c(3, 5, 7, Inf)
+
+slope_breaks <- c(seq(min(sim_data$a.slope), max(sim_data$a.slope), length.out = 3), Inf)
+
+beta_breaks <- c(seq(min(sim_data$beta), max(sim_data$beta), length.out = 3), Inf)
+
+lambda_breaks <- c(seq(min(sim_data$lambda), max(sim_data$lambda), length.out = 3), Inf)
+
+parameter_data <- sim_data |>
+  mutate(
+    OV_level      = cut(OV, breaks = OV_breaks, labels = level_labels, ordered_result = TRUE),
+    slope_degress = cut(a.slope, breaks = slope_breaks, labels = level_labels, right = FALSE),
+    beta_level = cut(beta, breaks = beta_breaks, labels = level_labels, ordered_result = TRUE, right = FALSE),
+    lambda_level = cut(lambda, breaks = lambda_breaks, labels = level_labels,  ordered_result = TRUE, right = FALSE)
+  )
