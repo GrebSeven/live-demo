844764a0d5dd491b3bafbeb8dc3cb58c5027d885
Greb <iblakethompson@gmail.com>
2026-03-30 18:38:59 +0100

    Major: - Made New plots for Threshold and Collapse. - Made all plots look pretty
    Minor:
    - Added more grouping varibales to Secondary dataframes
    - fixed some case issues
    - Made levels all ordered factors

--- Sim Analysis.R
+++ Sim Analysis.R
@@ -34,14 +34,17 @@ stimuli_data <- sim_data |>
   ) |>
   group_by(OV, DIFF, Resp) |>
   summarise(
-    Count = n(),
+    count = n(),
     mean_rt = mean(Time)
   ) |>
   pivot_wider(
     names_from = Resp,
-    values_from = c(Count, mean_rt)
+    values_from = c(count, mean_rt)
   ) |>
-  mutate(accuracy = as.numeric(Count_correct / (Count_correct + Count_incorrect)))
+  mutate(total_count = count_correct + count_incorrect,
+         accuracy = as.numeric(count_correct / (count_correct + count_incorrect)),
+         mean_rt = (mean_rt_correct * (count_correct / total_count)) +
+           (mean_rt_incorrect * (count_incorrect / total_count)))
 ## Accuracy Plots ------------------------------------------------------------------
 ggplot(data = stimuli_data, aes(x = OV, y = accuracy, colour = DIFF)) +
   geom_smooth(method = "loess") +
@@ -54,37 +57,55 @@ ggplot(data = stimuli_data, aes(x = OV, y = accuracy, colour = DIFF)) +
   theme_minimal() +
   guides(colour = (guide_legend(reverse = TRUE))) +
   labs(
-    title = "Decision Accuracy of Simulations by Magnitude, Differentiated by Alternative Value Difference",
+    title = "Simulation Decision Accuracy by Magnitude",
+    subtitle = "Coloured by Alternative Value Difference",
     x = "Magnitude",
     y = "Accuracy",
     colour = "Value Difference"
   )
-
-ggsave("accuracy_plot.pdf",
-  width = 21, height = 15,
-  units = "cm", path = "~/UoL/Live-DeMo"
-)
-
-## Response Time ---------------------------------------------------------------
+## Response Time Plots ---------------------------------------------------------------
 ### Overall Response Time PLot----
-ggplot(data = sim_data, aes(x = OV, y = Time, colour = DIFF)) +
+ggplot(data = stimuli_data, aes(x = OV, y = mean_rt, colour = DIFF)) +
   geom_smooth(method = "loess") +
   geom_point() +
-  facet_wrap(~Resp) +
-  theme_minimal()
+  scale_x_continuous(breaks = seq(3, 9, by = 1)) +
+  theme_minimal() +
+  labs(
+    title = "Simulation Mean Decision Response Time by Magnitude",
+    subtitle = "Coloured by Alternative Value Difference",
+    x = "Magnitude",
+    y = "Response Time (seconds)",
+    colour = "Value Difference"
+  )
 
 ### Mean Correct Response Time Plot----
 ggplot(data = stimuli_data, aes(x = OV, y = mean_rt_correct, colour = DIFF)) +
   geom_smooth(method = "loess") +
   geom_point() +
-  theme_minimal()
+  scale_x_continuous(breaks = seq(3, 9, by = 1)) +
+  theme_minimal()+
+  labs(
+    title = "Simulation Mean Correct Response Time by Magnitude",
+    subtitle = "Coloured by Alternative Value Difference",
+    x = "Magnitude",
+    y = "Response Time (seconds)",
+    colour = "Value Difference"
+  )
 
 ### Mean Error Response Time
 
 ggplot(data = stimuli_data, aes(x = OV, y = mean_rt_incorrect, colour = DIFF)) +
   geom_smooth(method = "loess") +
   geom_point() +
-  theme_minimal()
+  scale_x_continuous(breaks = seq(3, 9, by = 1)) +
+  theme_minimal()+
+  labs(
+    title = "Simulation Mean Incorrect Response Time by Magnitude",
+    subtitle = "Coloured by Alternative Value Difference",
+    x = "Magnitude",
+    y = "Response Time (seconds)",
+    colour = "Value Difference"
+  )
 
 # Parameter Data ---------------------------------------------------------------
 level_labels <- c("low", "med", "high")
@@ -95,6 +116,8 @@ OV_breaks <- c(seq(min(sim_data$OV), max(sim_data$OV), length.out = 3), Inf)
 
 diff_breaks <- c(seq(min(sim_data$DIFF), max(sim_data$DIFF), length.out = 3), Inf)
 
+threshold_breaks <- c(seq(min(sim_data$a.intercept), max(sim_data$a.intercept), length.out = 3), Inf)
+
 slope_breaks <- c(seq(min(sim_data$a.slope), max(sim_data$a.slope), length.out = 3), Inf)
 
 beta_breaks <- c(seq(min(sim_data$beta), max(sim_data$beta), length.out = 3), Inf)
@@ -102,22 +125,26 @@ beta_breaks <- c(seq(min(sim_data$beta), max(sim_data$beta), length.out = 3), In
 lambda_breaks <- c(seq(min(sim_data$lambda), max(sim_data$lambda), length.out = 3), Inf)
 
 parameter_data <- sim_data |>
-  filter(DIFF != 0) |>
   mutate(
     Resp = case_when( # Changing them for Clarity
       Resp == 1 ~ "correct",
       Resp == 2 ~ "incorrect"
     ),
     OV_level = cut(OV, breaks = OV_breaks, labels = level_labels, ordered_result = TRUE, right = FALSE),
-    diff_level = cut(DIFF, breaks = diff_breaks, labels = diff_labels, ordered_result = TRUE, right = FALSE),
-    slope_degrees = cut(a.slope, breaks = slope_breaks, labels = level_labels, right = FALSE),
+    diff_level = case_when(
+      DIFF == 0 ~ "equal",
+      TRUE ~ as.character(cut(DIFF, breaks = diff_breaks, labels = diff_labels, ordered_result = TRUE, right = FALSE)
+    )) |>
+      factor(levels = c("equal", diff_labels), ordered = TRUE),
+    threshold_level = cut(a.intercept, breaks = threshold_breaks, labels = level_labels, ordered_result = TRUE, right = FALSE),
+    collapse_rate = cut(a.slope, breaks = slope_breaks, labels = level_labels, ordered_result = TRUE, right = FALSE),
     beta_level = cut(beta, breaks = beta_breaks, labels = level_labels, ordered_result = TRUE, right = FALSE),
     lambda_level = cut(lambda, breaks = lambda_breaks, labels = level_labels, ordered_result = TRUE, right = FALSE),
-    across(c(Resp, DIFF), as.factor),
+    across(c(Resp, DIFF), as.factor)
   )
 
 beta_lambda_data <- parameter_data |>
-  group_by(beta_level, lambda_level, OV, OV_level, diff_level, Resp) |>
+  group_by(beta_level, lambda_level, OV, OV_level, diff_level, threshold_level, collapse_rate, Resp) |>
   summarise(
     count = n(),
     mean_rt = mean(Time)
@@ -134,28 +161,119 @@ beta_lambda_data <- parameter_data |>
     ratio = as.numeric(mean_rt / accuracy)
   )
 
-## Test Plots ---------------------------------------------------------------
-
-ggplot(parameter_data, aes(y = Time, x = beta_level, colour = OV_level)) +
-  geom_boxplot(outliers = FALSE) +
-  facet_wrap(~lambda_level)
+## Lambda:Beta Plots ---------------------------------------------------------------
 
 PLOT_acc_beta_lambda <-
   ggplot(beta_lambda_data, aes(x = OV, y = accuracy, colour = beta_level:lambda_level)) +
     geom_smooth() +
-    facet_wrap(~diff_level)
+    facet_wrap(~diff_level,
+               axes = "all",
+               labeller = as_labeller(
+                 c(equal = "Equal Values",
+                   difficult = "Difficult (Close Values)",
+                   medium = "Medium",
+                   easy = "Easy (Far Values)"
+               ))) +
+  scale_y_continuous(
+    breaks = seq(.4, 1, by = .1),
+    labels = scales::percent
+  ) +
+  scale_x_continuous(breaks = seq(3, 9, by = 1)) +
+  theme_minimal() +
+  labs(
+    title = "Simulation Accuracy by Decision Magnitude",
+    subtitle = "Coloured by Beta:Lambda Levels, Faceted by Decision Difficulty",
+    x = "Magnitude",
+    y = "Accuracy",
+    colour = "Beta Level:Lambda Level"
+  )
 
+PLOT_acc_beta_lambda
 
 PLOT_rt_beta_lambda <-
   ggplot(beta_lambda_data, aes(x = OV, y = mean_rt, colour = beta_level:lambda_level)) +
     geom_smooth() +
-    facet_wrap(~diff_level)
+    facet_wrap(~diff_level,
+               axes = "all",
+               labeller = as_labeller(
+                 c(equal = "Equal Values",
+                   difficult = "Difficult (Close Values)",
+                   medium = "Medium",
+                   easy = "Easy (Far Values)"
+                 ))) +
+  scale_y_continuous(
+    breaks = seq(.2, 3, by = 0.2)
+  )+
+  scale_x_continuous(breaks = seq(3, 9, by = 1)) +
+  theme_minimal() +
+  labs(title = "Mean Simulation Response Time by Decision Magnitude",
+       subtitle = "Coloured by Beta:Lambda Levels, Faceted by Decision Difficulty",
+       x = "Magnitude",
+       y = "Mean Response Time (Seconds)",
+       colour = "Beta Level:Lambda Level"
+       )
+  
+PLOT_rt_beta_lambda
 
-PLOT_ratio_beta_lambda <-
-  ggplot(beta_lambda_data, aes(x = OV, y = ratio, colour = beta_level:lambda_level)) +
-    geom_smooth()+
-  facet_wrap(~diff_level)
+## Threshold Plots----
 
-ggarrange(PLOT_acc_beta_lambda, PLOT_rt_beta_lambda,
-          labels = c("Accuracy", "Response Time", "Speed Accuracy Trade-off"),
-          ncol = 1, nrow = 3)
+PLOT_acc_thresholds <-
+  ggplot(data = beta_lambda_data, aes(x = OV,y = accuracy, colour = threshold_level:collapse_rate)) +
+  geom_smooth(method = "loess",
+              se = FALSE) +
+  facet_grid(rows = vars(beta_level),
+             cols = vars(lambda_level),
+             scales = "fixed",
+             axes = "all",
+             labeller = labeller(.rows = as_labeller(
+               c(low = "Low Leak",
+                 med = "Medium Leak",
+                 high = "High Leak")
+             ), .cols = as_labeller(
+               c(low = "Low Inhibition",
+                 med = "Medium Inhibition",
+                 high = "High Inhibiiton")
+             ))) +
+  scale_x_continuous(breaks = seq(3, 9, by = 1)) +
+  scale_y_continuous(
+    breaks = seq(.4, 1, by = .05),
+    labels = scales::percent
+  ) +
+  theme_minimal() +
+  labs(
+    title = "Simulation Accuracy by Decision Magnitude",
+    subtitle = "Faceted by Inhibition and Leak Levels, coloured by Threshold and Collapse Rate",
+    x = "Magnitude",
+    y = "Accuracy",
+    colour = "Threshold:Collapse Rate"
+  )
+
+PLOT_acc_thresholds
+
+PLOT_rt_thresholds <-
+  ggplot(data = beta_lambda_data, aes(x = OV,y = mean_rt, colour = threshold_level:collapse_rate)) +
+  geom_smooth(method = "loess",
+              se = FALSE) +
+  facet_grid(rows = vars(beta_level),
+             cols = vars(lambda_level),
+             scales = "fixed",
+             axes = "all",
+             labeller = labeller(.rows = as_labeller(
+               c(low = "Low Leak",
+                 med = "Medium Leak",
+                 high = "High Leak")
+             ), .cols = as_labeller(
+               c(low = "Low Inhibition",
+                 med = "Medium Inhibition",
+                 high = "High Inhibition")))) +
+  scale_x_continuous(breaks = seq(3, 9, by = 1)) +
+  theme_minimal() +
+  labs(
+    title = "Simulation Response Time by Decision Magnitude",
+    subtitle = "Faceted by Inhibition and Leak Levels, coloured by Threshold and Collapse Rate",
+    x = "Magnitude",
+    y = "Response Time (seconds)",
+    colour = "Threshold:Collapse Rate"
+  )
+
+PLOT_rt_thresholds
