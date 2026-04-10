90b288170a50ce20941dfba48b856adce9265a11
Greb <iblakethompson@gmail.com>
2026-04-10 13:53:13 +0100

    Made assignment of cores consistent between automation files

--- automated_plot.R
+++ automated_plot.R
@@ -1,6 +1,6 @@
 # SET HOW MNAY CORES TO USE!!!!! ----------------------------------
 
-how_many_cores = 3
+how_many_cores <- 3
 
 # Load in Packages--------------------------------------
 library(tidyverse)
